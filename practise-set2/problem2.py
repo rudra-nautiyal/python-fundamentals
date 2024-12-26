be6b20be4516bb46132522b4e9bf6dbc886036ea
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2024-12-27 00:33:09 +0530

    Python fundamentals

---
+++ practise-set2/problem2.py
@@ -0,0 +1,4 @@
+a = int(input("Dividend: "))
+b = int(input("Divisor: "))
+
+print("The reminder after dividing the two numbers is", a % b)
---
+++ practise-set2/problem3.py
@@ -0,0 +1,3 @@
+randomVariable = input("Enter any random datatype: ")
+
+print("The datatype you entered is", type(randomVariable))
